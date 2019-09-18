ce7762bf28dab20b76fd7af763d30cd4e9df57e7
Elijah Caluya <elijah.caluya@macys.com>
2019-09-18 16:22:38 -0700

    started JokeServer and client

--- JokeClient.java
+++ JokeClient.java
@@ -3,7 +3,7 @@ import java.net.*;	// For Java networking libraries
 
 
 // Class for client to connect to server
-public class InetClient 
+public class JokeClient 
 {
 
 	public static void main (String args[])
@@ -13,19 +13,19 @@ public class InetClient
 		if (args.length < 1) serverName = "localhost";
 		else serverName = args[0];
 
-		System.out.println("Clark Elliott's Inet Client, 1.8.\n");
-		System.out.println("using server: " + serverName + ", Port: 1565");
 		// Initialize the buffer to read in user input
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 	
+		
+
 		try {
 			String name;
 			do {
-				System.out.print("Enter a hostname or an IP address, (quit) to end: ");
+				System.out.print("Enter name: ");
 				System.out.flush();				// Make sure everything written to Standard out is sent
 				name = in.readLine();			// Set String variable to string line read in from buffer
 				if (name.indexOf("quit") < 0)	// Check if string entered is not "quit"
-					getRemoteAddress(name, serverName);	// If not quit, call getRemoteAddress() method to read output from server
+					getMessage(name, serverName);	// If not quit, call getRemoteAddress() method to read output from server
 			// While the input is not quit, keep reading in line and sending info to server
 			} while (name.indexOf("quit") < 0);			// Else, print out message and then end program.
 				System.out.println("Cancelled by user request.");
@@ -33,20 +33,9 @@ public class InetClient
 	}
 
 
-	// Method for converting byte IP address to string 
-	static String toText (byte ip[])
-	{
-		StringBuffer result = new StringBuffer();
-		for (int i = 0; i < ip.length; ++i){
-			if (i > 0) result.append(".");
-				result.append(0xff & ip[i]);
-		}
-		return result.toString();
-	}
-
 
 	// Method for sending and receiving data to server from client
-	static void getRemoteAddress(String name, String serverName)
+	static void getMessage(String name, String serverName)
 	{
 		Socket sock;					// Socket variable for connection to server
 		BufferedReader fromServer;		// Buffer variable to store output from server
@@ -67,13 +56,11 @@ public class InetClient
 			toServer.flush();
 
 
-			// Read in the print line from the server three times, since the server sends back 3 lines of output 
-			// Each time, print the output on a new line
-			for (int i = 1; i <= 3; i++){
-				textFromServer = fromServer.readLine();		// Read in one line from the output from server
-				// If the output from server is not null, print the output on a new line.
-				if (textFromServer != null) System.out.println(textFromServer);
-			}
+			
+			textFromServer = fromServer.readLine();		// Read in one line from the output from server
+			// If the output from server is not null, print the output on a new line.
+			if (textFromServer != null) System.out.println(textFromServer);
+			
 			// Close connection with server
 			sock.close();
 		// If there are any errors, print out error message
--- JokeServer.java
+++ JokeServer.java
@@ -7,6 +7,7 @@ class Worker extends Thread
 {
 
 	Socket sock;		// Create local variable for socket object
+	Boolean isJoke = true;
 	Worker (Socket s) {sock = s;}		// Worker class constructor makes the local variable sock to Socket argument given
 
 	// Override Thread.run() method for our own implementation 
@@ -25,9 +26,9 @@ class Worker extends Thread
 				String name;
 				// readLine method converts input from buffer into String and we set that to the variable "name"
 				name = in.readLine();
-				System.out.println("Looking up " + name);		// Print looking up hostname/address in server
-				// Call printRemoteAddress to write to client the info about hostname/address
-				printRemoteAddress(name, out);
+				System.out.println("Got from client: " + name);	
+				
+				sendMessage(out,this.isJoke);
 			} catch (IOException x) {
 				// Throw error if there is a problem reading from the buffer from the socket
 				System.out.println("Server read error");
@@ -40,43 +41,18 @@ class Worker extends Thread
 		} catch (IOException ioe) {System.out.println(ioe);}
 	}
 
-
-	// Method for looking up address from client and sending back info back to client
-	static void printRemoteAddress (String name, PrintStream out)
-	{
-		try {
-			// Send back to client message that server is looking up hostname
-			out.println("Looking up " + name + "...");
-			// Set IP Address object to be the IP address of the given host from client
-			// If nothing is sent over then localhost info will be set to InetAddress variable
-			InetAddress machine = InetAddress.getByName(name);
-			// Send back to client the hostname that the getHostName() method returns from InetAddress object
-			out.println("Host name : " + machine.getHostName());
-			// Send back to client the IP address that the getAddress() method returns from InetAddress object
-			out.println("Host IP : " + toText(machine.getAddress()));
-		// If unable to get the info from the hostname/address, send back to client failed message
-		} catch (UnknownHostException ex) {
-			out.println("Failed in attempt to look up " + name);
-		}
+	public void sendMessage(PrintStream out, Boolean joke){
+		if (joke)
+			out.println("Why did the chicken cross the road?");
+		else
+			out.println("One who sits on toilet, gets high on pot");
 	}
 
-
-	// Method for converting byte IP address to string 
-	static String toText(byte ip[])
-	{
-		StringBuffer result = new StringBuffer();
-
-		for (int i = 0; i <  ip.length; ++i){
-			if (i > 0) result.append(".");
-					result.append(0xff & ip[i]);
-		}
-		return result.toString();
-	}
 }
 
 
 // Class for server
-public class InetServer 
+public class JokeServer 
 {
 	public static void main(String args[]) throws IOException 
 	{
@@ -86,8 +62,7 @@ public class InetServer
 
 		// Construct server socket on set port and with max queue length for incoming connection requests
 		ServerSocket servsock = new ServerSocket(port, q_len);
-
-		System.out.println("Clark Elliott's Inet server 1.8 starting up, listening at port 1565.\n");
+		System.out.println("Server is running...");
 
 		while (true) {						// Infinite loop:
 			sock = servsock.accept();		// Wait for client connection
