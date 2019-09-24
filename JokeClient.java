6520f9090806362b04647604f3b7e3671d386344
Elijah Caluya <elicaluya@yahoo.com>
2019-09-23 23:25:34 -0700

    adding server client connections

--- JokeClient.java
+++ JokeClient.java
@@ -1,90 +1,116 @@
 import java.io.*; 	// For Input and Ouput
 import java.net.*;	// For Java networking libraries
-import java.util.*;	// For Scanner
+import java.util.*;
 
 
 // Class for client to connect to server
 public class JokeClient 
 {
 
-	static void getMessage(Socket sock)
+	static HashSet<String> jokeState = new HashSet<>();
+	static HashSet<String> proverbState = new HashSet<>();
+
+	public static void main (String args[])
 	{
-		BufferedReader fromServer;		// Buffer variable to store output from server
-		String textFromServer;
+		String serverName;
+		// If User doesn't provide a server name in arguments, then set servername to localhost
+		if (args.length < 1) serverName = "localhost";
+		else serverName = args[0];
 
+		System.out.println("Clark Elliott's Inet Client, 1.8.\n");
+		System.out.println("using server: " + serverName + ", Port: 1565");
+		// Initialize the buffer to read in user input
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String name = "";
+
+	
 		try {
-			// Read the output from the server through the socket and store the data into the buffer
-			fromServer = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			System.out.println("Enter your name: ");
+			name = in.readLine();
 
-			textFromServer = fromServer.readLine();		// Read in one line from the output from server
-			// If the output from server is not null, print the output on a new line.
-			System.out.println(textFromServer);
-			sock.close();
-		} catch (IOException i) {
+		} catch (IOException i){
 			System.out.println(i);
 		}
 
 
+		try {
+			String request;
+			do {
+				System.out.print("Press enter to send request to server or 'quit': ");
+				System.out.flush();				// Make sure everything written to Standard out is sent
+				request = in.readLine();			// Set String variable to string line read in from buffer
+				if (request.indexOf("quit") < 0)	// Check if string entered is not "quit"
+					getRequest(name, serverName, jokeState, proverbState);	// If not quit, call getRequest() method to read output from server
+			// While the input is not quit, keep reading in line and sending info to server
+			} while (name.indexOf("quit") < 0);			// Else, print out message and then end program.
+				System.out.println("Cancelled by user request.");
+		} catch (IOException x) {x.printStackTrace();}		// Print data in output console
+	}
+
+
+	// Method for converting byte IP address to string 
+	static String toText (byte ip[])
+	{
+		StringBuffer result = new StringBuffer();
+		for (int i = 0; i < ip.length; ++i){
+			if (i > 0) result.append(".");
+				result.append(0xff & ip[i]);
+		}
+		return result.toString();
 	}
 
 
 	// Method for sending and receiving data to server from client
-	static void getConnection(String name)
+	static void getRequest(String name, String serverName, HashSet<String> jokeSet, HashSet<String> proverbSet)
 	{
 		Socket sock;					// Socket variable for connection to server
-		PrintStream stringToServer;			// PrintStrem variable to store output to the server
-		DataOutputStream intToServer;
-		
+		BufferedReader messageFromServer;		// Buffer variable to store output from server
+		PrintStream nameToServer;			// PrintStrem variable to store output to the server
+		ObjectInputStream stateFromServer;
+		ObjectOutputStream stateToServer;
+		String textmessageFromServer;			// String variable to store output from server after converted to String
 
 		try {
-			sock = new Socket("localhost", 4545);	// Connect to client at given server name and port
+			sock = new Socket(serverName, 1565);	// Connect to client at given server name and port
 
-			
+			// Read the output from the server through the socket and store the data into the buffer
+			messageFromServer = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			// Stream for reading in objects a.k.a our states
+			stateFromServer = new ObjectInputStream(sock.getInputStream());
+
+
 			// Set up output stream to go to the server from the socket
-			stringToServer = new PrintStream(sock.getOutputStream());
-
-			intToServer = new DataOutputStream(sock.getOutputStream());
-
-
-			intToServer.writeInt(0);
-			intToServer.flush();
+			nameToServer = new PrintStream(sock.getOutputStream());
+			// Stream for sending out objects a.k.a our states
+			stateToServer = new ObjectOutputStream(sock.getOutputStream());
 
 
 			// Send name to Server where the server will read in the line
-			stringToServer.println(name);
+			nameToServer.println(name);
 			// Make sure that everything sent over to server is sent out
-			stringToServer.flush();
+			nameToServer.flush();
+
+			stateToServer.writeObject(jokeSet);
+			stateToServer.writeObject(proverbSet);
+			stateToServer.flush();
+
+
 			
-			getMessage(sock);
-			
+			textmessageFromServer = messageFromServer.readLine();		// Read in one line from the output from server
+			// If the output from server is not null, print the output on a new line.
+			if (textmessageFromServer != null) System.out.println(textmessageFromServer);
+
+			try {
+				jokeState = (HashSet<String>) stateFromServer.readObject();
+				proverbState = (HashSet<String>) stateFromServer.readObject();
+			} catch (ClassNotFoundException c) {System.out.println(c);}
+
+			// Close connection with server
+			sock.close();
 		// If there are any errors, print out error message
 		} catch (IOException x) {
 			System.out.println("Socket error.");
 			x.printStackTrace();		// Print out IOException data to console log
 		}
 	}
-
-
-
-	public static void main (String args[])
-	{
-		String name;
-
-
-		// Initialize the buffer to read in user input
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-	
-		System.out.print("Enter name: ");
-		System.out.flush();				// Make sure everything written to Standard out is sent
-
-		
-		try {
-			name = in.readLine();
-			getConnection(name);
-		} catch (IOException x) {x.printStackTrace();}		// Print data in output console
-
-		
-
-	}
-
 }
--- JokeServer.java
+++ JokeServer.java
@@ -1,5 +1,6 @@
 import java.io.*; 	// For Input and Ouput
 import java.net.*;	// For Java networking libraries
+import java.util.*;
 
 
 // Worker Class is a subclass of built in Thread class to use Thread functionality
@@ -7,147 +8,140 @@ class Worker extends Thread
 {
 
 	Socket sock;		// Create local variable for socket object
-	String message;
-	Worker (Socket s, String m) {sock = s; message = m;}		// Worker class constructor makes the local variable sock to Socket argument given
+	static Boolean isJoke;
+	Worker (Socket s, Boolean j) {sock = s; isJoke = j;}		// Worker class constructor makes the local variable sock to Socket argument given
 
 	// Override Thread.run() method for our own implementation 
 	public void run()
 	{
 		PrintStream out = null;		// PrintStream variable so we can write data to Output Stream back to the client
+		BufferedReader in = null;	// Create a buffer to read text from character-input stream from the client
+		ObjectInputStream stateFromClient;
+		ObjectOutputStream stateToClient;
+
+		HashSet<String> jokeState = new HashSet<>();
+		HashSet<String> proverbState = new HashSet<>();
 
 		try {
+			// Initialize buffer variable to read the input stream from the client connection
+			in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			// Stream for getting the state of the client
+			stateFromClient = new ObjectInputStream(sock.getInputStream());
+
 			// Initialize our PrintStream variable to write to the OutputStream of the socket (back to the client)
 			out = new PrintStream(sock.getOutputStream());
-			
-			out.println(message);
-			sock.close();
+			// Stream for sending back to client the state after update
+			stateToClient = new ObjectOutputStream(sock.getOutputStream());
 
+			try {
+				String name;
+				// readLine method converts input from buffer into String and we set that to the variable "name"
+				name = in.readLine();
+				System.out.println("Got name from client: " + name);		// Print looking up hostname/address in server
+
+				try {
+					jokeState = (HashSet<String>)stateFromClient.readObject();
+					proverbState = (HashSet<String>)stateFromClient.readObject();
+				} catch (ClassNotFoundException c){System.out.println(c);}
+
+				// Call sendMessage to write to client the info about hostname/address
+				sendMessage(name, out, stateToClient, jokeState, proverbState);
+			} catch (IOException x) {
+				// Throw error if there is a problem reading from the buffer from the socket
+				System.out.println("Server read error");
+				x.printStackTrace();		// Print out IOException data to console log
+			}
+
+			// Close the connection with the socket
+			sock.close();
 		// Print error if there is problem initializing variables for our input and output streams
 		} catch (IOException ioe) {System.out.println(ioe);}
+	}
+
+
+	static String getJoke(HashSet<String> set, int size)
+	{
+		String[] jokes = {"JA","JB","JC","JD"};
+		if (size == 4){
+			size = 0;
+			set.clear();
+		}
+		return jokes[size];
 
 	}
 
+	static String getProverb(HashSet<String> set, int size)
+	{
+		String[] proverbs = {"PA","PB","PC","PD"};
+		if (size == 4){
+			size = 0;
+			set.clear();
+		}
+		return proverbs[size];
+	}	
+
+	// Method for looking up address from client and sending back info back to client
+	static void sendMessage (String name, PrintStream messageOut, ObjectOutputStream stateToClient, HashSet<String> jState, HashSet<String> pState)
+	{
+		try {
+			String message = "";
+			if (isJoke){
+				message = getJoke(jState, jState.size());
+				jState.add(message);
+			}
+			else{
+				message = getProverb(pState, pState.size());
+				pState.add(message);
+			}
+
+			// Send Joke or proverb back to client
+			messageOut.println(message);
+			messageOut.flush();
+
+			stateToClient.writeObject(jState);
+			stateToClient.writeObject(pState);
+			stateToClient.flush();
+			
+		// If unable to get the info from the hostname/address, send back to client failed message
+		} catch (IOException i) {
+			System.out.println(i);
+		}
+	}
+
+
+	// Method for converting byte IP address to string 
+	static String toText(byte ip[])
+	{
+		StringBuffer result = new StringBuffer();
+
+		for (int i = 0; i <  ip.length; ++i){
+			if (i > 0) result.append(".");
+					result.append(0xff & ip[i]);
+		}
+		return result.toString();
+	}
 }
 
 
 // Class for server
 public class JokeServer 
 {
-
-	public static int getJokeProverbIndex(int index)
-	{
-		index++;
-		if (index == 4)
-			index = 0;
-		return index;
-	}
-
-
-	public static String getInfo(Socket sock)
-	{
-		BufferedReader inClient = null;	// Create a buffer to read text from character-input stream from the client
-		DataInputStream intFromClient;
-		String input = "";
-		int connectionType;
-
-
-
-		try {
-			inClient = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-			intFromClient = new DataInputStream(sock.getInputStream());
-			connectionType = intFromClient.readInt();
-
-			if (connectionType == 0){
-				System.out.println("Got connection from Client");
-				input = inClient.readLine();
-			}
-			else if (connectionType == 1){
-				System.out.println("Got connection from Admin");
-				input = inClient.readLine();
-			}
-
-
-		} catch (IOException i){
-			System.out.println(i);
-		}
-
-		return input;
-	}
-
-
-
-	public static Boolean setMode (Socket sock, String message)
-	{
-		PrintStream out = null;		// PrintStream variable so we can write data to Output Stream back to the client
-		Boolean jokeMode = true;
-		String toClient = "";
-
-		if (message.contains("joke")){
-			jokeMode = true;
-			toClient = "Set Mode to joke";
-			System.out.println("Set Mode to joke");
-		}
-		else if (message.contains("proverb")){
-			jokeMode = false;
-			toClient = "Set Mode to proverb";
-			System.out.println("Set Mode to proverb");
-		}
-
-
-		try {
-			// Initialize our PrintStream variable to write to the OutputStream of the socket (back to the client)
-			out = new PrintStream(sock.getOutputStream());
-			
-			out.println(toClient);
-			out.flush();
-
-		// Print error if there is problem initializing variables for our input and output streams
-		} catch (IOException ioe) {System.out.println(ioe);}
-
-		return jokeMode;
-	}
-
-
 	public static void main(String args[]) throws IOException 
 	{
 		int q_len = 6;			// Number of requests for OpSys to queue
-		int port = 4545;		// Port number we will use
+		int port = 1565;		// Port number we will use
 		Socket sock;			// Socket variable to connect with client
 		Boolean isJoke = true;
-		int jokeIndex = -1, proverbIndex = -1;
-		String infoFromClient, name;
-
-		String[] joke_array = new String[]{"JA","JB","JC","JD"};
-		String[] proverb_array = new String[]{"PA","PB","PC","PD"};
 
 		// Construct server socket on set port and with max queue length for incoming connection requests
 		ServerSocket servsock = new ServerSocket(port, q_len);
-		System.out.println("Server is running...");
 
-
+		System.out.println("Clark Elliott's Inet server 1.8 starting up, listening at port 1565.\n");
 
 		while (true) {						// Infinite loop:
 			sock = servsock.accept();		// Wait for client connection
-
-			infoFromClient = getInfo(sock);
-
-			if (!infoFromClient.equals("joke") && !infoFromClient.equals("proverb")){
-				name = infoFromClient;
-				System.out.println("Name from client: " + name);
-				if (isJoke){
-					jokeIndex = getJokeProverbIndex(jokeIndex);
-					new Worker(sock,joke_array[jokeIndex]).start();
-				}
-				else {
-					proverbIndex = getJokeProverbIndex(proverbIndex);
-					new Worker(sock,proverb_array[proverbIndex]).start();
-				}
-			} else {
-				isJoke = setMode(sock,infoFromClient);
-			}
-
-			
-				
+			// Create new Worker thread once client connection is accepted and do code in run() method with start() call
+			new Worker(sock, isJoke).start();		
 		}
 	}
 }
